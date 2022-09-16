da9514064c647fc2fd73ef25bf0ed039eca5cd40
Lucas Capucci <lucascapucci29@gmail.com>
2022-09-17 07:27:12 +0900

    Teste de Link

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ import Link from 'next/link';
 
 function Home(){
     return (<div>
-        <h1>HOME</h1>
+        <h1>Home</h1>
 
-        <Link href="/about">
+        <Link href="/sobre">
         <a>Acessar página Sobre</a>
         </Link>
     </div>)
 }
-
 
 export default Home
