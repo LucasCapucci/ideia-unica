7adfede12c33265f01aeac26c580187d7405bcb7
Lucas Capucci <lucascapucci29@gmail.com>
2022-09-17 10:51:02 +0900

    usa variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,14 @@
-function tempo(request, response){
-    const dynamicDate = new Date();    
-
+async function tempo(request, response){
+    const apiSecret = process.env.CONVERT_API_SECRET;
+    const dynamicDate = new Date();  
+    
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
     response.json({
-        date: dynamicDate.toGMTString()    
-    })
+        date: dynamicDate.toGMTString(), 
+        inscritos: inscritos  
+    });
 }
 
 export default tempo;
